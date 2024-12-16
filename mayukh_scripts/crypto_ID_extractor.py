c85466a63ed09af28ff2f44ee678a98ffc8c3f39
CelestiaLKamI <work.mayukh14@gmail.com>
2024-12-16 18:18:43 +0530

    minor changes

--- mayukh_scripts/crypto_ID_extractor.py
+++ mayukh_scripts/crypto_ID_extractor.py
@@ -2,52 +2,6 @@ import nuke
 import ast
 from PySide2.QtWidgets import QWidget, QPushButton, QComboBox, QLabel, QVBoxLayout, QHBoxLayout
 
-def extract_channels():
-    """
-    Extracts all unique channel layers from selected 'Read' nodes
-    and creates Shuffle nodes for each layer.
-    """
-    try:
-        # Get selected nodes of type 'Read'
-        nodes = nuke.selectedNodes(filter='Read')
-        
-        if not nodes:
-            # Show message if no 'Read' nodes are selected
-            nuke.message("Please select a 'Read' node/s")
-            return
-
-        for read in nodes:
-            # Get all channel names in the node
-            channels_list_all = read.channels()
-            
-            channels_list_each = []
-            
-            # Extract unique layer names from channels
-            for each in channels_list_all:
-                layer_name = each.split('.')[0]
-                if not layer_name in channels_list_each:
-                    channels_list_each.append(layer_name)
-
-            for each in channels_list_each:
-                # Create Shuffle node for each unique layer
-                shuffle = nuke.createNode('Shuffle')
-                shuffle['in'].setValue(each)
-                shuffle.setXpos(read.xpos() + ((channels_list_each.index(each) + 1) * 200))
-                shuffle.setYpos(read.ypos() + 20)
-                shuffle['label'].setValue("<b>[value in]</b>")
-                shuffle['note_font_color'].setValue(int("FFFFFFFF", 16))
-                
-                # Deselect the last node in the list
-                if channels_list_each[-1] == each:
-                    shuffle['selected'].setValue(False)
-                
-                # Connect the first Shuffle node to the Read node
-                elif channels_list_each[0] == each:
-                    shuffle.setInput(0, read)
-    except Exception as e:
-        # Display any errors that occur
-        nuke.message(str(e))
-
 def extract_crypto():
     """
     Extracts Cryptomatte data from the metadata of the selected node in Nuke
@@ -131,10 +85,13 @@ def extract_crypto():
                         crypto_node["matteList"].setValue(each_id)
                         crypto_node["cryptoLayerChoice"].setValue(self.comboBox.currentIndex())
                         crypto_node.setYpos(self.node.ypos() + ((id_list.index(each_id) + 1) * 200))
+                        crypto_node["label"].setValue(each_id)
 
+                window.close()
+                
         global window
         window = MainWindow()
-        window.setWindowTitle("Crypto Extractor - By Mayukh")
+        window.setWindowTitle("Crypto Extractor")
         window.show()
 
     except Exception as e:
--- menu.py
+++ menu.py
@@ -1,11 +1,10 @@
 print("Loading menu.py...")
 
-
 menu = nuke.menu("Nuke")
 my_scripts = menu.addMenu("Mayukh Scripts")
 
-
-from channel_and_crypto_extractor import *
 extractor = my_scripts.addMenu("Extractor")
+from channel_extractor import extract_channels
 extractor.addCommand("Extract Channels", extract_channels, 'shift+e')
+from crypto_ID_extractor import extract_crypto
 extractor.addCommand("Extract Crypto IDs", extract_crypto)
