33f6b041abe1f2aeed7b4c0d70159582b5c079f6
Mayukh Mitra <156567859+CelestiaLKamI@users.noreply.github.com>
2024-12-20 22:17:06 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
+from crypto_id_extractor import extract_crypto
+from channel_extractor import extract_channels
+
 print("Loading menu.py...")
 
 menu = nuke.menu("Nuke")
 my_scripts = menu.addMenu("Mayukh Scripts")
 
 extractor = my_scripts.addMenu("Extractor")
-from channel_extractor import extract_channels
 extractor.addCommand("Extract Channels", extract_channels, 'shift+e')
-from crypto_ID_extractor import extract_crypto
 extractor.addCommand("Extract Crypto IDs", extract_crypto)
